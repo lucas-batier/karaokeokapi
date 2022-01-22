0099ab77786f0b89d5b34ca535631af9e06e2757
lucasbat20 <lucas.batier@hotmail.fr>
2022-01-22 19:15:49 +0100

    Add heroku app in allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['karaokeokapi.herokuapp.com']
 
 
 # Application definition
